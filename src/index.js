fffa0981dd829e8ce102ee1f5df706b08b41c75e
codemitrayt <codemitrayt@gmail.com>
2025-04-13 16:41:42 +0530

    fix: issues

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ const tokenService = new TokenService()
 const app = express()
 
 const corsOption = {
-  origin: ENV.FRONTEND_URL,
   origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
