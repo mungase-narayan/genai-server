632328e98a1fb4f2a01dcbd4412b4b09d1bae1f8
codemitrayt <codemitrayt@gmail.com>
2025-05-19 13:46:21 +0530

    fix: issues

--- src/index.js
+++ src/index.js
@@ -25,6 +25,13 @@ if (ENV.NODE_ENV === 'production') {
   app.use(enforce.HTTPS({ trustProtoHeader: true }))
 }
 
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https') {
+    return res.redirect(`https://${req.hostname}${req.url}`)
+  }
+  next()
+})
+
 const corsOption = {
   origin: '*',
   credentials: true,
